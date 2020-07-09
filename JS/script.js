66dce31eeacdd59b49b7797a484ad190ffac936b
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2020-07-09 13:19:49 +0200

    Add Sidelist with opacity function

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
+const sideBar = document.querySelector(".sideBar");
 
 const handleClick = () => {
     hamburger.classList.toggle("hamburger-active");
+    sideBar.classList.toggle("sideBar-active");
 }
 
 hamburger.addEventListener("click", handleClick);
